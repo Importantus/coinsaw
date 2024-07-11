377c4dac6fd13082229e1faf5a8b4f54c8473f17
Importantus <importantus@mailbox.org>
2024-07-11 21:50:45 +0200

    :lipstick: Make extraSmall BorderShape bigger

--- app/src/main/java/digital/fischers/coinsaw/ui/theme/Theme.kt
+++ app/src/main/java/digital/fischers/coinsaw/ui/theme/Theme.kt
@@ -1,15 +1,10 @@
 package digital.fischers.coinsaw.ui.theme
 
-import android.app.Activity
-import android.os.Build
-import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 
 private val DarkColorScheme = darkColorScheme(
     primary = sunnyYellow,
@@ -34,6 +29,9 @@ fun CoinsawTheme(
     val colorScheme = DarkColorScheme
 
     MaterialTheme(
+        shapes = MaterialTheme.shapes.copy(
+            extraSmall = RoundedCornerShape(10.dp)
+        ),
         colorScheme = colorScheme,
         typography = Typography,
         content = content
