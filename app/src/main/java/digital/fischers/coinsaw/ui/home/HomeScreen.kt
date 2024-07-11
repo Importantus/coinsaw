33d78572b041586f97cc9c26aa018dd6c6838c3d
Importantus <importantus@mailbox.org>
2024-07-11 21:57:35 +0200

    :zap: Use LazyColumn for displaying the list of groups

--- app/src/main/java/digital/fischers/coinsaw/ui/home/HomeScreen.kt
+++ app/src/main/java/digital/fischers/coinsaw/ui/home/HomeScreen.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -72,12 +73,13 @@ fun HomeScreen(
     }) {
         Column {
             if (groups.isNotEmpty()) {
-                Column(
+                LazyColumn(
                     modifier = Modifier
                         .fillMaxWidth(),
                     verticalArrangement = Arrangement.spacedBy(16.dp)
                 ) {
-                    groups.forEach { group ->
+                    items(groups.size) {
+                        val group = groups[it]
                         GroupCard(
                             groupUiState = group,
                             onClick = { onGroupClicked(group.id) }
