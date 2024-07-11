3edc1fb5b380e2da89543f2b9c0a710cc5ced38c
Importantus <importantus@mailbox.org>
2024-07-11 21:59:38 +0200

    :construction: Add new routes for viewing, adding and editing groupmembers

--- app/src/main/java/digital/fischers/coinsaw/ui/CoinsawAppState.kt
+++ app/src/main/java/digital/fischers/coinsaw/ui/CoinsawAppState.kt
@@ -18,6 +18,18 @@ sealed class Screen(val route: String) {
         fun createRoute(groupId: String) = "group/$groupId/settings"
     }
 
+    data object GroupMemberList : Screen("group/{$ARG_GROUP_ID}/memberList") {
+        fun createRoute(groupId: String) = "group/$groupId/memberList"
+    }
+
+    data object GroupCreateMember : Screen("group/{$ARG_GROUP_ID}/createMember") {
+        fun createRoute(groupId: String) = "group/$groupId/createMember"
+    }
+
+    data object GroupEditMember : Screen("group/{$ARG_GROUP_ID}/editMember/{$ARG_USER_ID}") {
+        fun createRoute(groupId: String, userId: String) = "group/$groupId/editMember/$userId"
+    }
+
     data object NewGroup : Screen("newGroup")
 
     data object NewGroupSettings : Screen("newGroup/settings")
@@ -30,12 +42,17 @@ sealed class Screen(val route: String) {
         fun createRoute(groupId: String) = "newGroup/$groupId/memberList"
     }
 
+    data object NewGroupEditMember : Screen("newGroup/{$ARG_GROUP_ID}/editMember/{$ARG_USER_ID}") {
+        fun createRoute(groupId: String, userId: String) = "newGroup/$groupId/editMember/$userId"
+    }
+
     data object AddBill : Screen("group/{$ARG_GROUP_ID}/addItem") {
         fun createRoute(groupId: String) = "group/$groupId/addItem"
     }
 
     companion object {
         const val ARG_GROUP_ID = "groupId"
+        const val ARG_USER_ID = "userId"
     }
 }
 
@@ -52,7 +69,11 @@ class CoinsawAppState(
     private val context: Context
 ) {
     fun navigateHome() {
-        navController.navigate(Screen.Home.route)
+        if(navController.previousBackStackEntry?.destination?.route != Screen.Home.route) {
+            navController.navigate(Screen.Home.route)
+        } else {
+            navController.popBackStack()
+        }
     }
 
     fun navigateToGroup(groupId: String) {
@@ -61,6 +82,18 @@ class CoinsawAppState(
 
     fun navigateToGroupSettings(groupId: String) {
         navController.navigate(Screen.GroupSettings.createRoute(groupId))
+    }
+
+    fun navigateToGroupMemberList(groupId: String) {
+        navController.navigate(Screen.GroupMemberList.createRoute(groupId))
+    }
+
+    fun navigateToGroupCreateMember(groupId: String) {
+        navController.navigate(Screen.GroupCreateMember.createRoute(groupId))
+    }
+
+    fun navigateToGroupEditMember(groupId: String, userId: String) {
+        navController.navigate(Screen.GroupEditMember.createRoute(groupId, userId))
     }
 
     fun navigateToAddItem(groupId: String) {
@@ -73,6 +106,14 @@ class CoinsawAppState(
 
     fun navigateToNewGroupCreateMember(groupId: String) {
         navController.navigate(Screen.NewGroupCreateMember.createRoute(groupId))
+    }
+
+    fun navigateToNewGroupMemberList(groupId: String) {
+        navController.navigate(Screen.NewGroupMemberList.createRoute(groupId))
+    }
+
+    fun navigateToNewGroupEditMember(groupId: String, userId: String) {
+        navController.navigate(Screen.NewGroupEditMember.createRoute(groupId, userId))
     }
 
     fun navigateBack() {
