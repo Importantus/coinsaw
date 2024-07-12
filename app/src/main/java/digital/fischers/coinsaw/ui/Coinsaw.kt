73390d499c097f6a9c694e8c40e1f71d8cbf1d13
Importantus <importantus@mailbox.org>
2024-07-12 13:13:33 +0200

    :dizzy: Implement new page transitions

--- app/src/main/java/digital/fischers/coinsaw/ui/Coinsaw.kt
+++ app/src/main/java/digital/fischers/coinsaw/ui/Coinsaw.kt
@@ -1,8 +1,15 @@
 package digital.fischers.coinsaw.ui
 
+import androidx.compose.animation.AnimatedContentTransitionScope
 import androidx.compose.animation.EnterTransition
 import androidx.compose.animation.ExitTransition
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.scaleIn
+import androidx.compose.animation.scaleOut
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
@@ -24,102 +31,145 @@ import digital.fischers.wisesplitpocv2.ui.rememberCoinsawAppState
 fun CoinsawApp(
     appState: CoinsawAppState = rememberCoinsawAppState()
 ) {
-    NavHost(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(MaterialTheme.colorScheme.background),
-        navController = appState.navController,
-        startDestination = Screen.Home.route,
-//        enterTransition = { EnterTransition.None },
-//        exitTransition = { ExitTransition.None }
-    ) {
-        composable(Screen.Home.route) {
-            HomeScreen(onGroupAddClicked = { appState.navigateToNewGroupName() },
-                onGroupJoinClicked = { /*TODO*/ },
-                onGroupClicked = { groupId ->
-                    appState.navigateToGroup(groupId)
-                })
-        }
+    Box(modifier = Modifier.fillMaxSize()) {
+        val forwardAnimationDuration = 300
+        val backwardAnimationDuration = 300
+        NavHost(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(MaterialTheme.colorScheme.background),
+            navController = appState.navController,
+            startDestination = Screen.Home.route,
+            enterTransition = {
+                val initialDeep = getRouteDeepness(initialState.destination.route ?: "")
+                val targetDeep = getRouteDeepness(targetState.destination.route ?: "")
 
-        navigation(
-            route = Screen.NewGroup.route,
-            startDestination = Screen.NewGroupSettings.route,
-        ) {
-            composable(Screen.NewGroupSettings.route) {
-                NewGroupScreen(onNavigateBack = { appState.navigateHome() },
-                    onNavigateForward = { appState.navigateToNewGroupCreateMember(it) })
-            }
-
-            composable(Screen.NewGroupCreateMember.route) {
-                AddMemberScreen(onBackNavigation = {
-                    appState.navController.navigate(route = Screen.Home.route) {
-                        popUpTo(route = Screen.Home.route) { inclusive = false }
-                    }
-                }, onForwardNavigation = {
-                    appState.navigateToNewGroupMemberList(it)
-                }, isInGroupCreationFlow = true
-                )
-            }
-
-            composable(Screen.NewGroupEditMember.route) {
-                EditMemberScreen(
-                    onBackNavigation = {
-                        appState.navigateBack()
-                    },
-                    isInGroupCreationFlow = true
-                )
-            }
-
-            composable(Screen.NewGroupMemberList.route) {
-                MemberlistScreen(onBackNavigation = {
-                    appState.navController.navigate(route = Screen.Home.route) {
-                        popUpTo(route = Screen.Home.route) { inclusive = false }
-                    }
-                }, onMemberAddClicked = {
-                    appState.navigateToNewGroupCreateMember(it)
-                }, onMemberClicked = { groupId, memberId ->
-                    appState.navigateToNewGroupEditMember(groupId, memberId)
-                }, isInGroupCreationFlow = true, onForwardNavigation = {
-                    appState.navController.navigate(route = Screen.Group.createRoute(it)) {
-                        popUpTo(route = Screen.Home.route) { inclusive = false }
-                    }
-                })
-            }
-        }
-
-        composable(Screen.Group.route) {
-            GroupScreen(onBackNavigation = { appState.navigateHome() },
-                onGroupSettingsClicked = {},
-                onGroupMembersClicked = {
-                    appState.navigateToGroupMemberList(it)
-                },
-                onAddMemberClicked = {
-                    appState.navigateToGroupCreateMember(it)
-                },
-                onBillClicked = {},
-                onAddBillClicked = {},
-                onAddTransactionClicked = {},
-                onSettleUpClicked = {})
-        }
-
-        composable(Screen.GroupCreateMember.route) {
-            AddMemberScreen(onBackNavigation = { appState.navigateBack() },
-                onForwardNavigation = { appState.navigateBack() })
-        }
-
-        composable(Screen.GroupMemberList.route) {
-            MemberlistScreen(onBackNavigation = { appState.navigateBack() },
-                onMemberAddClicked = { appState.navigateToGroupCreateMember(it) },
-                onMemberClicked = { groupId, memberId ->
-                    appState.navigateToGroupEditMember(
-                        groupId,
-                        memberId
+                when {
+                    initialDeep < targetDeep -> slideIntoContainer(
+                        AnimatedContentTransitionScope.SlideDirection.Left,
+                        animationSpec = tween(forwardAnimationDuration)
                     )
-                })
-        }
 
-        composable(Screen.GroupEditMember.route) {
-            EditMemberScreen(onBackNavigation = { appState.navigateBack() })
+                    initialDeep > targetDeep -> {
+                        (scaleIn(
+                            initialScale = 0.98f,
+                            animationSpec = tween(backwardAnimationDuration))
+                        + fadeIn(animationSpec = tween(backwardAnimationDuration)))
+                    }
+                    else -> EnterTransition.None
+                }
+            },
+            exitTransition = {
+                val initialDeep = getRouteDeepness(initialState.destination.route ?: "")
+                val targetDeep = getRouteDeepness(targetState.destination.route ?: "")
+
+                when {
+                    initialDeep > targetDeep -> slideOutOfContainer(
+                        AnimatedContentTransitionScope.SlideDirection.Right,
+                        animationSpec = tween(backwardAnimationDuration)
+                    )
+                    initialDeep < targetDeep -> {
+                        (scaleOut(
+                            targetScale = 0.98f,
+                            animationSpec = tween(forwardAnimationDuration))
+                        + fadeOut(animationSpec = tween(forwardAnimationDuration)))
+                    }
+                    else -> ExitTransition.None
+                }
+            },
+        ) {
+            composable(Screen.Home.route) {
+                HomeScreen(onGroupAddClicked = { appState.navigateToNewGroupName() },
+                    onGroupJoinClicked = { /*TODO*/ },
+                    onGroupClicked = { groupId ->
+                        appState.navigateToGroup(groupId)
+                    })
+            }
+
+            navigation(
+                route = Screen.NewGroup.route,
+                startDestination = Screen.NewGroupSettings.route,
+            ) {
+                composable(Screen.NewGroupSettings.route) {
+                    NewGroupScreen(onNavigateBack = { appState.navigateHome() },
+                        onNavigateForward = { appState.navigateToNewGroupCreateMember(it) })
+                }
+
+                composable(Screen.NewGroupCreateMember.route) {
+                    AddMemberScreen(onBackNavigation = {
+                        appState.navController.navigate(route = Screen.Home.route) {
+                            popUpTo(route = Screen.Home.route) { inclusive = false }
+                        }
+                    }, onForwardNavigation = {
+                        appState.navigateToNewGroupMemberList(it)
+                    }, isInGroupCreationFlow = true
+                    )
+                }
+
+                composable(Screen.NewGroupEditMember.route) {
+                    EditMemberScreen(
+                        onBackNavigation = {
+                            appState.navigateBack()
+                        },
+                        isInGroupCreationFlow = true
+                    )
+                }
+
+                composable(Screen.NewGroupMemberList.route) {
+                    MemberlistScreen(onBackNavigation = {
+                        appState.navController.navigate(route = Screen.Home.route) {
+                            popUpTo(route = Screen.Home.route) { inclusive = false }
+                        }
+                    }, onMemberAddClicked = {
+                        appState.navigateToNewGroupCreateMember(it)
+                    }, onMemberClicked = { groupId, memberId ->
+                        appState.navigateToNewGroupEditMember(groupId, memberId)
+                    }, isInGroupCreationFlow = true, onForwardNavigation = {
+                        appState.navController.navigate(route = Screen.Group.createRoute(it)) {
+                            popUpTo(route = Screen.Home.route) { inclusive = false }
+                        }
+                    })
+                }
+            }
+
+            composable(Screen.Group.route) {
+                GroupScreen(onBackNavigation = { appState.navigateHome() },
+                    onGroupSettingsClicked = {},
+                    onGroupMembersClicked = {
+                        appState.navigateToGroupMemberList(it)
+                    },
+                    onAddMemberClicked = {
+                        appState.navigateToGroupCreateMember(it)
+                    },
+                    onBillClicked = {},
+                    onAddBillClicked = {},
+                    onAddTransactionClicked = {},
+                    onSettleUpClicked = {})
+            }
+
+            composable(Screen.GroupCreateMember.route) {
+                AddMemberScreen(onBackNavigation = { appState.navigateBack() },
+                    onForwardNavigation = { appState.navigateBack() })
+            }
+
+            composable(Screen.GroupMemberList.route) {
+                MemberlistScreen(onBackNavigation = { appState.navigateBack() },
+                    onMemberAddClicked = { appState.navigateToGroupCreateMember(it) },
+                    onMemberClicked = { groupId, memberId ->
+                        appState.navigateToGroupEditMember(
+                            groupId,
+                            memberId
+                        )
+                    })
+            }
+
+            composable(Screen.GroupEditMember.route) {
+                EditMemberScreen(onBackNavigation = { appState.navigateBack() })
+            }
         }
     }
+}
+
+fun getRouteDeepness(route: String): Int {
+    return route.split("/").size
 }
