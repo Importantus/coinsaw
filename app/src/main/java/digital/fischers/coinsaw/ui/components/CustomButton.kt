f939d77a9556d0004c7c9d706862c4b252d524ea
Importantus <importantus@mailbox.org>
2024-08-12 22:47:27 +0200

    :lipstick: Pass the background color of the CustomButton as parameter

--- app/src/main/java/digital/fischers/coinsaw/ui/components/CustomButton.kt
+++ app/src/main/java/digital/fischers/coinsaw/ui/components/CustomButton.kt
@@ -14,6 +14,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -27,6 +28,7 @@ import androidx.compose.ui.unit.sp
 fun CustomButton(
     onClick: () -> Unit,
     modifier: Modifier = Modifier,
+    color: Color = MaterialTheme.colorScheme.surface,
     @DrawableRes icon: Int? = null,
     text: String? = null,
 ) {
@@ -37,7 +39,7 @@ fun CustomButton(
                 onClick = onClick,
                 role = Role.Button
             )
-            .background(MaterialTheme.colorScheme.surface)
+            .background(color)
             .padding(horizontal = 8.dp, vertical = 7.dp),
         verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.spacedBy(4.dp)
