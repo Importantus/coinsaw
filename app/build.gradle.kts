80d945f9f26e55c65620a3587e7bdd3a18bb1212
Importantus <importantus@mailbox.org>
2024-08-12 22:44:35 +0200

    :heavy_plus_sign: Add qr-code dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -70,6 +70,7 @@ dependencies {
     implementation(libs.androidx.room.runtime)
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
+    implementation(libs.qrcode.kotlin)
     ksp(libs.androidx.room.compiler)
     implementation(libs.androidx.room.ktx)
 
