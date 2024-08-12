e5d497c430ad03f5cddca448312317e398aa188a
Importantus <importantus@mailbox.org>
2024-08-12 22:52:42 +0200

    :alien: Add name to sessions

--- app/src/main/java/digital/fischers/coinsaw/data/remote/Session.kt
+++ app/src/main/java/digital/fischers/coinsaw/data/remote/Session.kt
@@ -1,7 +1,8 @@
 package digital.fischers.coinsaw.data.remote
 
 data class CreateSessionRequest(
-    val token: String
+    val token: String,
+    val name: String
 )
 
 data class CreateSessionResponse(
@@ -9,10 +10,13 @@ data class CreateSessionResponse(
     val creation_timestamp: String,
     val last_active_timestamp: String,
     val id: String,
-    val token: String
+    val token: String,
+    val group: GroupWithoutToken,
+    val name: String
 )
 
 data class Session(
+    val name: String,
     val admin: Boolean,
     val creation_timestamp: String,
     val last_active_timestamp: String,
