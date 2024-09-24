5913e4ec1da73ed5ed4d32b49b328712c49bbefe
Importantus <importantus@mailbox.org>
2024-09-24 18:56:33 +0200

    🔖 v0.0.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "digital.fischers.coinsaw"
         minSdk = 24
         targetSdk = 34
-        versionCode = 7
-        versionName = "0.0.6"
+        versionCode = 8
+        versionName = "0.0.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
